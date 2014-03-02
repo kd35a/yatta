5ebea1317732f495eb59994bbef4741582118d6d
Fredrik Strandin <fredrik@strandin.name>
2014-03-02 10:57:00 +0000

    Refactores Somethings-factory

--- app/js/factories.js
+++ app/js/factories.js
@@ -1,22 +1,28 @@
+/*
+ * Somethings factory
+ *
+ * Just a useless factory to get started with AngularJS
+ */
 yatta.factory('Somethings', function () {
 	var factory = {};
+	var storageTag = 'somethings';
 
 	factory.get = function () {
-		var somethings = JSON.parse(localStorage.getItem('somethings') || '[]');
+		var somethings = JSON.parse(localStorage.getItem(storageTag) || '[]');
 		return somethings;
 	};
 	factory.add = function (data) {
-		var somethings = JSON.parse(localStorage.getItem('somethings') || '[]');
+		var somethings = JSON.parse(localStorage.getItem(storageTag) || '[]');
 		if ($.inArray(data, somethings) < 0) {
 			somethings.push(data);
-			localStorage.setItem('somethings', JSON.stringify(somethings));
+			localStorage.setItem(storageTag, JSON.stringify(somethings));
 			return true;
 		} else {
 			return false;
 		}
 	};
 	factory.clearAll = function () {
-		localStorage.setItem('somethings', '[]');
+		localStorage.setItem(storageTag, '[]');
 	};
 
 	return factory;
